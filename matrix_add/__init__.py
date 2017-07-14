59167ce4e8e6dce7c535c4bbef918c6067af3c63
PatWie <mail@patwie.com>
2017-07-14 13:09:34 +0200

    fix cmake

--- matrix_add/__init__.py
+++ matrix_add/__init__.py
@@ -7,7 +7,7 @@ from tensorflow.python.framework import ops
 
 __all__ = ['matrix_add', 'matrix_add_grad']
 
-path = os.path.join(os.path.dirname(__file__), 'build/matrix_add_op.so')
+path = os.path.join(os.path.dirname(__file__), 'matrix_add_op.so')
 _matrix_add_module = tf.load_op_library(path)
 
 matrix_add = _matrix_add_module.matrix_add
@@ -29,4 +29,17 @@ Example:
 
     from matrix_add import matrix_add
     matrix_add(...)
+
+
+
+
+nvcc -std=c++11 --expt-relaxed-constexpr --shared --gpu-architecture=sm_52 \
+    -c -o matrix_add_op.cu.o kernels/matrix_add_kernel.cu \
+    -isystem /home/wieschol/.local/lib/python2.7/site-packages/tensorflow/include \
+    -D GOOGLE_CUDA=1 -x cu -Xcompiler -fPIC --shared -D _GLIBCXX_USE_CXX11_ABI=1 
+g++ -std=c++11 -o matrix_add_op.so --shared \
+    matrix_add_op.cu.o kernels/matrix_add_op.cc ops/matrix_add.cc kernels/matrix_add_kernel.cc\
+    -isystem /home/wieschol/.local/lib/python2.7/site-packages/tensorflow/include  \
+    -lcudart -L/usr/local/cuda/lib64 \
+    -fPIC --shared -D _GLIBCXX_USE_CXX11_ABI=1 
 """
