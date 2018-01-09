5072c836eae8e1cda5a0a1a4c64310c6b9386f8f
PatWie <mail@patwie.com>
2018-01-09 14:14:59 +0100

    working cmake version

--- matrix_add/cmake/Modules/FindTensorFlow.cmake
+++ matrix_add/cmake/Modules/FindTensorFlow.cmake
@@ -1,5 +1,14 @@
+# University Tuebingen, 2018
 include(FindPackageHandleStandardArgs)
 unset(TENSORFLOW_FOUND)
+
+
+# get TF include path
+execute_process(
+    COMMAND "python" -c
+            "try: import tensorflow as tf; print tf.sysconfig.get_include() \nexcept:pass\n"
+    OUTPUT_VARIABLE GUESSED_TF_INC)
+message(STATUS "GUESSED_TF_INC: ${GUESSED_TF_INC}")
 
 find_path(TensorFlow_INCLUDE_DIR
         NAMES
@@ -7,18 +16,27 @@ find_path(TensorFlow_INCLUDE_DIR
         tensorflow/cc
         third_party
         HINTS
-        /home/wieschol/.local/lib/python2.7/site-packages/tensorflow/include
+        ${GUESSED_TF_INC}
         /usr/local/include/google/tensorflow
         /usr/include/google/tensorflow)
 
+
+execute_process(
+    COMMAND "python" -c
+            "import tensorflow as tf; print(tf.sysconfig.get_lib())\n"
+    OUTPUT_VARIABLE GUESSED_TF_LIB)
+message(STATUS "GUESSED_TF_LIB: ${GUESSED_TF_LIB}")
+
+
 find_library(TensorFlow_LIBRARY NAMES libtensorflow_framework.so
         HINTS
+        ${GUESSED_TF_LIB}
         /usr/lib
-        /home/wieschol/.local/lib/python2.7/site-packages/tensorflow
         /usr/local/lib)
 
 # set TensorFlow_FOUND
 find_package_handle_standard_args(TensorFlow DEFAULT_MSG TensorFlow_INCLUDE_DIR TensorFlow_LIBRARY)
+
 
 # set external variables for usage in CMakeLists.txt
 if(TENSORFLOW_FOUND)
@@ -28,20 +46,7 @@ if(TENSORFLOW_FOUND)
     message(STATUS "TensorFlow_INCLUDE_DIRS: ${TensorFlow_INCLUDE_DIR}")
 endif()
 
+
 # hide locals from GUI
 mark_as_advanced(TensorFlow_INCLUDE_DIR TensorFlow_LIBRARY)
 
-# Locates the tensorFlow library and include directories.
-
-# # get TF include path
-# execute_process(
-#     COMMAND "python" -c
-#             "try: import tensorflow as tf; print tf.sysconfig.get_include() \nexcept:pass\n"
-#     OUTPUT_VARIABLE TF_INC)
-# message(STATUS "TF_INC: ${TF_INC}")
-
-# execute_process(
-#     COMMAND "python" -c
-#             "import tensorflow as tf; print(tf.sysconfig.get_lib())\n"
-#     OUTPUT_VARIABLE TF_LIB)
-# message(STATUS "TF_LIB: ${TF_LIB}")
