0a5da881681d1a604face58a9a9192a155a59ae5
PatWie <mail@patwie.com>
2018-03-17 14:12:37 +0100

    update to TF v1.7 (remove redundant dependency to tensorflow:cuda)

--- matrix_add/configure.py
+++ matrix_add/configure.py
@@ -7,6 +7,9 @@ if '__cxx11_abi_flag__' not in dir(tf):
     sys.exit(1)
 
 with open("tensorflow_config.txt", "w") as f:
+    print("TensorFlow_ABI: {}".format(tf.__cxx11_abi_flag__))
     f.write("set(TensorFlow_ABI %i)\n" % tf.__cxx11_abi_flag__)
+    print("TensorFlow_INCLUDE_DIRS: {}".format(tf.sysconfig.get_include()))
     f.write("set(TensorFlow_INCLUDE_DIRS \"%s\")\n" % tf.sysconfig.get_include())
+    print("TensorFlow_LIBRARIES: {}".format(tf.sysconfig.get_lib()))
     f.write("set(TensorFlow_LIBRARIES \"%s/libtensorflow_framework.so\")\n" % tf.sysconfig.get_lib())
